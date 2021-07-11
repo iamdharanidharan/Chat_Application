2eedd4dce478f261860ae5649e21795412cb844c
Dharanidharan Mani <74301964+iamdharanidharan@users.noreply.github.com>
2021-07-11 23:58:44 +0530

    Added passport and express session

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 
@@ -10,10 +12,19 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 app.set('view engine','pug');
 
 app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug', {title : 'Hello', message : 'Please Login'});
+  res.render(process.cwd() + '/views/pug', {title : 'Hello', message : 'Please Login', assetpath : req.headers.host});
 });
 
 const PORT = process.env.PORT || 3000;
