3bd3e2cc5d33ca8aca9c281a21bc3f5713b2075c
Dharanidharan Mani <74301964+iamdharanidharan@users.noreply.github.com>
2021-07-12 00:55:50 +0530

    added and used passport-local

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const passport = require('passport');
 const session = require('express-session');
 const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -33,6 +34,18 @@ myDB(async (client) => {
       message: 'Please login'
     });
   });
+
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log('User '+ username +' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
 
   // Serialization and deserialization
   passport.serializeUser((user, done) => {
